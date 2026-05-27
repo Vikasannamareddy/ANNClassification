64ee381230dfc52a35a87332f62d8d762febadea
Vikasannamareddy <137205525+Vikasannamareddy@users.noreply.github.com>
2026-05-27 22:21:52 +0530

    Delete run_streamlit.bat

--- run_streamlit.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "C:\Users\Vikas\OneDrive\Desktop\GenAI\Python\annclassification"
-call C:\Users\Vikas\OneDrive\Desktop\GenAI\Python\.venv\Scripts\activate.bat
-python -m streamlit run ANNapp.py
-pause
